a9c8c6594876bc8d769e2705ec73aec02987c7c2
Herbert Ilhan Tanujaya <mail@herbert.id>
2021-11-07 18:58:12 +0800

    Make a chess board show up

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react"],
+  plugins: ["import", "react"],
   rules: {
     "import/order": [
       "error",
--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
+const withTM = require("next-transpile-modules")(["react-chessground"]);
+
+module.exports = withTM({
   reactStrictMode: true,
-};
+  experimental: {
+    esmExternals: "loose",
+  },
+});
